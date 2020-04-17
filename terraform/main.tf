e7273163c02aea303a459d60fad9214cd09b79ad
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2020-04-17 09:46:34 -0300

    Event Horizon infra update
    - Added route tables
    - Added internet gateways

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,10 @@ terraform {
   }
 }
 
+// ============================================================================================== //
+// VPCs
+// ============================================================================================== //
+
 resource "aws_vpc" "eh-sa-east-1-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -21,6 +25,10 @@ resource "aws_vpc" "eh-sa-east-1-vpc" {
     Name = "eh-sa-east-1-vpc"
   }
 }
+
+// ============================================================================================== //
+// Subnets
+// ============================================================================================== //
 
 resource "aws_subnet" "eh-lab-sa-east-1a-public-subnet" {
   vpc_id                          = aws_vpc.eh-sa-east-1-vpc.id
@@ -42,6 +50,53 @@ resource "aws_subnet" "eh-lab-sa-east-1a-private-subnet" {
   map_public_ip_on_launch         = false
 
   tags = {
-    "Name" = "eh-lab-sa-east-1a-private-subnet"
+    Name = "eh-lab-sa-east-1a-private-subnet"
+  }
+}
+
+// ============================================================================================== //
+// Route Tables
+// ============================================================================================== //
+
+resource "aws_route_table" "eh-lab-sa-east-1a-public-subnet-route-table" {
+  vpc_id = aws_vpc.eh-sa-east-1-vpc.id
+
+  route = [
+    {
+      cidr_block                = "0.0.0.0/0"
+      egress_only_gateway_id    = ""
+      gateway_id                = aws_internet_gateway.eh-sa-east-1-vpc-internet-gateway.id
+      instance_id               = ""
+      ipv6_cidr_block           = ""
+      nat_gateway_id            = ""
+      network_interface_id      = ""
+      transit_gateway_id        = ""
+      vpc_peering_connection_id = ""
+    }
+  ]
+
+  tags = {
+    Name = "eh-lab-sa-east-1a-public-subnet-route-table"
+  }
+}
+
+
+resource "aws_route_table" "eh-lab-sa-east-1a-private-subnet-route-table" {
+  vpc_id = aws_vpc.eh-sa-east-1-vpc.id
+
+  tags = {
+    Name = "eh-lab-sa-east-1a-private-subnet-route-table"
+  }
+}
+
+// ============================================================================================== //
+// Internet Gateways
+// ============================================================================================== //
+
+resource "aws_internet_gateway" "eh-sa-east-1-vpc-internet-gateway" {
+  vpc_id = aws_vpc.eh-sa-east-1-vpc.id
+
+  tags     = {
+    Name = "eh-sa-east-1-vpc-internet-gateway"
   }
 }
